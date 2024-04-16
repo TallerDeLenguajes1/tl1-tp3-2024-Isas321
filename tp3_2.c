f3f8d2c4247eb3362ce26c5d52d6927a0429b4a6
Javier Alfredo Isasmendi <javier.isas321@gmail.com>
2024-04-16 13:02:19 -0300

    punto2 - promedio de ganancias por anio

--- tp3_2.c
+++ tp3_2.c
@@ -17,7 +17,6 @@ void inicializadorMatrizRandom(int *matriz, int filas, int columnas, int min_val
     }
 }
 
-
 void mostrar_matriz(int *matriz, int filas, int columnas){
     for (int i = 0; i < filas; i++)
     {
@@ -30,12 +29,31 @@ void mostrar_matriz(int *matriz, int filas, int columnas){
     printf("\n\n");
 }
 
+void gananciaPorAnio(int *matriz, int anios, int meses){
+    int suma;
+    float promedio;
+    for (int i = 0; i < anios; i++)
+    {
+        suma=0;
+        promedio=suma/
+        printf("\n");
+        for (int j = 0; j < meses; j++)
+        {
+            suma= *(matriz+i*meses+j) + suma;
+        }
+        promedio=(float)suma/meses;
+        printf("\tLa ganancia promedio del anio %d es de: %.2f", i+1, promedio);
+    }
+    printf("\n\n");
+}
+
 int main(){
     int anios=5, meses=12;
     int matriz[anios][meses];
 
     inicializadorMatrizRandom(&matriz[0][0], anios, meses, 10000, 50000);
     mostrar_matriz(*matriz, anios, meses);
+    gananciaPorAnio(*matriz, anios, meses);
 
     return 0;
 }
