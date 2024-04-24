f5ccb58788744c25ba1a1af20c02a6862ba1cb01
Isas321 <javier.isas321@gmail.com>
2024-04-24 13:43:23 -0300

    se crea funcion para solicitar cant_clientes

--- tp3_4.c
+++ tp3_4.c
@@ -15,6 +15,24 @@ struct Cliente {
 int ClienteID; // Numerado en el ciclo iterativo
 char *NombreCliente; // Ingresado por usuario
 int CantidadProductosAPedir; // (aleatorio entre 1 y 5)
-struct Producto *Productos //El tamaño de este arreglo depende de la variable
+struct Producto *Productos; //El tamaño de este arreglo depende de la variable
 // “CantidadProductosAPedir”
 };
+
+void cant_clientes(int *n){
+  printf("\nCantidad de clientes que se deben ingresar: ");
+  scanf("%d", n); //Guardo en la posicion del puntero el valor ingresado
+  getchar();
+}
+
+
+
+int main(){
+  int n;
+  
+  cant_clientes(&n);
+  printf("%d", n);
+  
+  getchar();
+  return 0;
+}
