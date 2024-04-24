f121ba5399b39d478e62664a44f7860355cb5ad0
Isas321 <javier.isas321@gmail.com>
2024-04-24 13:33:14 -0300

    Creacion de punto 3.4 mas carga de estructuras

---
+++ tp3_4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *TiposProductos[]={"Galletas","Snack","Cigarrillos","Caramelos","Bebidas"};
+
+struct Producto {
+int ProductoID; //Numerado en ciclo iterativo
+int Cantidad; // entre 1 y 10
+char *TipoProducto; // Algún valor del arreglo TiposProductos
+float PrecioUnitario; // entre 10 - 100
+};
+
+struct Cliente {
+int ClienteID; // Numerado en el ciclo iterativo
+char *NombreCliente; // Ingresado por usuario
+int CantidadProductosAPedir; // (aleatorio entre 1 y 5)
+struct Producto *Productos //El tamaño de este arreglo depende de la variable
+// “CantidadProductosAPedir”
+};
