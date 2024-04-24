f2f75fb8cf7b7e01ab65029791f3ba25998e18ad
Isas321 <javier.isas321@gmail.com>
2024-04-24 14:12:43 -0300

    reserva dinamica para clientes

--- tp3_4.c
+++ tp3_4.c
@@ -11,6 +11,8 @@ char *TipoProducto; // Algún valor del arreglo TiposProductos
 float PrecioUnitario; // entre 10 - 100
 };
 
+typedef struct Producto Producto;
+
 struct Cliente {
 int ClienteID; // Numerado en el ciclo iterativo
 char *NombreCliente; // Ingresado por usuario
@@ -19,19 +21,32 @@ struct Producto *Productos; //El tamaño de este arreglo depende de la variable
 // “CantidadProductosAPedir”
 };
 
-void cant_clientes(int *n){
+typedef struct Cliente Cliente;
+
+/* void cant_clientes(int *n){
   printf("\nCantidad de clientes que se deben ingresar: ");
   scanf("%d", n); //Guardo en la posicion del puntero el valor ingresado
   getchar();
+} */
+
+int cant_clientes(){
+  int n;
+  printf("\nCantidad de clientes que se deben ingresar: ");
+  scanf("%d", &n); //Guardo en la posicion del puntero el valor ingresado
+  return n;
 }
 
-
+int generador_num_random(int min, int max){
+    return (min+rand()%(max-min+1));
+}
 
 int main(){
-  int n;
-  
-  cant_clientes(&n);
-  printf("%d", n);
+
+  int cantidad_clientes;
+
+  cantidad_clientes=cant_clientes();
+
+  Cliente *clientes=(Cliente *)malloc(sizeof(Cliente)*cantidad_clientes);
   
   getchar();
   return 0;
